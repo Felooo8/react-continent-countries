f5bc1c476fb93f56e8f4af8cd3d26b3455000c67
felo <felobanka@gmail.com>
2023-05-20 19:06:25 +0200

    Added Form with basic CSS

---
+++ src/Components/Form.tsx
@@ -0,0 +1,93 @@
+import { Button, Input, TextField } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import React from "react";
+
+const continents = {
+  AF: "Africa",
+  AS: "Asia",
+  EU: "Europe",
+  NA: "North America",
+  SA: "South America",
+  OC: "Oceania",
+  AN: "Antarctica",
+};
+
+interface Props {
+  toggleSelectContinent: (continent: string) => void;
+  selectedContinent: string;
+  toggleSetNumberOfCountries: (numberOfCountries: number) => void;
+  numberOfCountries: number;
+  toggleFetch: () => void;
+}
+
+export default function Form(props: Props) {
+  const handleChangeContinent = (event: SelectChangeEvent) => {
+    props.toggleSelectContinent(event.target.value);
+  };
+
+  const handleChangeNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value);
+    props.toggleSetNumberOfCountries(newValue);
+  };
+
+  const handleFetch = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    props.toggleFetch();
+  };
+
+  return (
+    <React.Fragment>
+      <form
+        onSubmit={handleFetch}
+        style={{
+          display: "grid",
+          margin: "auto",
+          maxWidth: "500px",
+        }}
+      >
+        <label>Select a continent</label>
+        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel>Continent</InputLabel>
+          <Select
+            autoWidth
+            value={props.selectedContinent}
+            label="Continent *"
+            onChange={handleChangeContinent}
+          >
+            <MenuItem value="">
+              <em>Select Continent</em>
+            </MenuItem>
+            {Object.entries(continents).map(([code, name]) => (
+              <MenuItem key={code} value={code}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText>Required</FormHelperText>
+        </FormControl>
+        <label>Number of countries to display</label>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel>Number of countries</InputLabel>
+
+          <Input
+            value={props.numberOfCountries}
+            size="small"
+            onChange={handleChangeNumber}
+            inputProps={{
+              min: 2,
+              max: 10,
+              type: "number",
+            }}
+          />
+        </FormControl>
+        <Button variant="contained" type="submit">
+          Get countries!
+        </Button>
+      </form>
+    </React.Fragment>
+  );
+}
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "../Components/Form";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [selectedContinent, setSelectedContinent] = useState<string>("");
+  const [numberOfCountries, setNumberOfCountries] = useState<number>(2);
+
+  const toggleSelectContinent = (continent: string) => {
+    setSelectedContinent(continent);
+  };
+
+  const toggleSetNumberOfCountries = (numberOfCountries: number) => {
+    setNumberOfCountries(numberOfCountries);
+  };
+
+  const fetchCountries = () => {
+    console.log(selectedContinent, numberOfCountries);
+  };
+
+  return (
+    <div style={{ padding: "10px" }}>
+      <h1>Discover Countries by Continent</h1>
+      <Form
+        selectedContinent={selectedContinent}
+        toggleSelectContinent={toggleSelectContinent}
+        numberOfCountries={numberOfCountries}
+        toggleSetNumberOfCountries={toggleSetNumberOfCountries}
+        toggleFetch={fetchCountries}
+      />
+    </div>
+  );
 }
